013ede8d221dbd26183389d33fcf9f61f60735b8
Damien Metzler <111077+dmetzler@users.noreply.github.com>
2024-10-04 15:42:10 +0200

    first import

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Welcome to your CDK TypeScript project
+# AWS Workshop IDE
 
-This is a blank project for CDK development with TypeScript.
+Now that Amazon Cloud9 is deprecated, you cannot start a Cloud9 instance on a new account, except the ones that are provisionned by AWS.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+This project helps deploying an EC2 instance with correct profile and SSH keys so that you can use easily the remote VS Code extension. You then benefit from the same kind of environment than Cloud9, but on your Laptop.
+
 
 ## Useful commands
 
@@ -12,3 +13,11 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk deploy`  deploy this stack to your default AWS account/region
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
+
+
+## Licensing
+
+Source code in Composable Doc is copyright Comnposable Doc and
+contributors, and licensed under the Apache License, Version 2.0.
+
+See the [LICENSE](LICENSE) file for details.
--- lib/aws-workshop-ide-stack.ts
+++ lib/aws-workshop-ide-stack.ts
@@ -1,16 +1,98 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as fs from 'fs';
+
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+interface WorkshopIdeStackProps extends cdk.StackProps {
+  clientIp: string;
+  publicKeyPath?: string;  
+  instanceClass?: ec2.InstanceClass;
+  instanceSize?: ec2.InstanceSize;
+  managedPolicy?: iam.IManagedPolicy;
+}
 
 export class AwsWorkshopIdeStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+  public instance:ec2.Instance;
+
+  constructor(scope: Construct, id: string, props: WorkshopIdeStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define default variables
+    if(props.instanceClass === undefined) {
+      props.instanceClass = ec2.InstanceClass.T2
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsWorkshopIdeQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    if(props.instanceSize === undefined) {
+      props.instanceSize = ec2.InstanceSize.MICRO
+    }
+
+    if(props.managedPolicy === undefined) {
+      props.managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')
+    }
+
+    if(props.publicKeyPath === undefined){
+      const homedir = require('os').homedir();
+      props.publicKeyPath = `${homedir}/.ssh/id_rsa.pub`  
+    }
+
+    // Get the default VPC
+    const defaultVpc = ec2.Vpc.fromLookup(this, 'VPC', { isDefault: true })
+
+    // Create instance role with Administrator Acces
+    const instanceRole = new iam.Role(this, 'workshop-instance-role', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      managedPolicies: [
+        props.managedPolicy,
+      ]
+    }) 
+    
+
+    const sshKeyPair = new ec2.CfnKeyPair(this, 'workshop-ssh-key', {
+      keyName: 'workshop-ssh-key',
+      publicKeyMaterial: fs.readFileSync(props.publicKeyPath).toString()
+    })
+
+    // Create Security group
+    const securityGroup = new ec2.SecurityGroup(this, 'workshop-security-group', {
+      vpc: defaultVpc,
+      allowAllOutbound: true,
+      securityGroupName: 'workshop-security-group'
+    })
+
+    // create ingress on ssh port
+    securityGroup.addIngressRule(ec2.Peer.ipv4(`${props.clientIp}/32`), ec2.Port.tcp(22), 'Allow SSH Access')
+    
+    // Create EC2 instance
+    const instance = new ec2.Instance(this, 'workshop-instance', {
+      vpc: defaultVpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2, 
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: new ec2.AmazonLinux2023ImageSsmParameter(),
+      vpcSubnets: { 
+        subnetType: ec2.SubnetType.PUBLIC 
+      },
+      role: instanceRole,
+      keyPair: ec2.KeyPair.fromKeyPairAttributes(this, 'KeyPair', {
+        keyPairName: sshKeyPair.keyName,
+        type: ec2.KeyPairType.RSA,
+      }),
+      userData: ec2.UserData.custom(`
+        #!/bin/bash
+        mkdir -p /home/ec2-user/environment
+        chown ec2-user:ec2-user /home/ec2-user/environment
+      `),
+      securityGroup: securityGroup
+    })
+
+    // Output the IP Address of the ec2 instance
+    new cdk.CfnOutput(this, 'InstancePublicIpAddress', {
+      value: instance.instancePublicDnsName
+    })
+
   }
 }
